3290e9491535cc4513923780255a6345560f811e
theji2bs <malidi.elhadad@gmail.com>
2016-02-17 13:32:33 +0100

    first run for header, nav and bottom

--- dist/footer.php
+++ dist/footer.php
@@ -35,4 +35,12 @@
 <div class="moncercle6 cercle "></div>
 <div class="moncercle6 cercle"></div>
 
+
+<div class="container-on-off">
+	<p class="left">On</p>
+	<p class="right">Off</p>
+</div>
+
+<!-- <div class="moncercle22  "><p class="txtcercle">On</p></div>
+<div class="moncercle23  "><p class="txtcercle">Off</p></div> -->
 </div>
