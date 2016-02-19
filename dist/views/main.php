8b3b1bf693443249f3484cb7e6f424d636e26b4a
theji2bs <root@MacBook-Pro-de-MALIDI.local>
2016-02-19 01:47:24 +0100

    add dev branch

--- dist/views/main.php
+++ dist/views/main.php
@@ -10,6 +10,8 @@
 	<div id="container" style="width:100%; height:400px;"></div>
 	</div>
 
+
+
 	<div class="mid-right playeur">
 		
 
@@ -23,7 +25,174 @@
 	</div>
 </div>
 	<div class="big-foot">
-	qsdfqsdfqdsfqsdf
+	
+<div class="ok">	
+<div class="sidebar">
+
+	<img src="assets/img/favoris.svg" class="imgsidebar">
+		<img src="assets/img/move.svg" class="imgsidebar">
+		<img src="assets/img/number.svg" class="imgsidebar">
+
+</div>
+
+
+
+<div class="pad">
+
+	<div class="txtpad">
+	
+	<p class="orange">PHONE</p> &nbsp; &nbsp;
+	<p>Callpad</p>
+
+</div>
+
+<div class="numpad">
+	<p>06 88 99 ... ...</p>
+</div>
+
+	<div class="one">
+	<div class="number">1</div>
+	<div class="number ">2</div>
+	<div class="number onee">3</div>
+	
+	</div>
+	<div class="two">
+	<div class="number haut">4</div>
+	<div class="number bas">5</div>
+	<div class="number bas">6</div>
+	</div>
+	<div class="tree">
+	<div class="number bas ">7</div>
+	<div class="number haut">8</div>
+	<div class="number haut">9</div>
+	</div>
+	<div class="four">
+	<div class="number bas">*</div>
+	<div class="number bas">0</div>
+	<div class="number bas">#</div>
+	</div>
+	
+
+
+	
+</div>
+
+
+<div class="historique">
+	<p class="strong">Martin Maillot</p>
+	<p class="petit">+0633445566</p>
+		<br><br>
+	<p class="strong">Martin Maillot</p>
+	<p class="petit">+0633445566</p>
+		<br><br>
+	<p class="strong">Martin Maillot</p>
+	<p class="petit">+0633445566<</p>
+
+</div>
+
+
+<div class="contactcontenu">
+	
+	<div class="contact">
+		<div class="los">
+	<img src="assets/img/hexagone.svg" class="losange">
+		</div>
+	<div class="cont">
+	<p class="strong">Martin Maillot</p>
+	<p class="petit">+0633445566</p>
+    </div>
+    <div class="bar"></div>
+		<div class="icons">
+		<img src="assets/img/phone.svg" class="iconegauche">
+		<img src="assets/img/message.svg" class="iconedroite">
+		</div>
+
+	</div>
+
+
+	<div class="contact">
+		<div class="los">
+	<img src="assets/img/hexagone.svg" class="losange">
+		</div>
+	<div class="cont">
+	<p class="strong">Martin Maillot</p>
+	<p class="petit">+0633445566</p>
+    </div>
+    <div class="bar"></div>
+		<div class="icons">
+		<img src="assets/img/phone.svg" class="iconegauche">
+		<img src="assets/img/message.svg" class="iconedroite">
+		</div>
+
+	</div>
+
+
+	<div class="contact">
+		<div class="los">
+	<img src="assets/img/hexagone.svg" class="losange">
+		</div>
+	<div class="cont">
+	<p class="strong">Martin Maillot</p>
+	<p class="petit">+0633445566</p>
+    </div>
+    <div class="bar"></div>
+		<div class="icons">
+		<img src="assets/img/phone.svg" class="iconegauche">
+		<img src="assets/img/message.svg" class="iconedroite">
+		</div>
+
+	</div>
+
+
+	<div class="contact">
+		<div class="los">
+	<img src="assets/img/hexagone.svg" class="losange">
+		</div>
+	<div class="cont">
+	<p class="strong">Martin Maillot</p>
+	<p class="petit">+0633445566</p>
+    </div>
+    <div class="bar"></div>
+		<div class="icons">
+		<img src="assets/img/phone.svg" class="iconegauche">
+		<img src="assets/img/message.svg" class="iconedroite">
+		</div>
+
+	</div>
+
+
+	<div class="contact">
+		<div class="los">
+	<img src="assets/img/hexagone.svg" class="losange">
+		</div>
+	<div class="cont">
+	<p class="strong">Martin Maillot</p>
+	<p class="petit">+0633445566</p>
+    </div>
+    <div class="bar"></div>
+		<div class="icons">
+		<img src="assets/img/phone.svg" class="iconegauche">
+		<img src="assets/img/message.svg" class="iconedroite">
+		</div>
+
+	</div>
+
+
+
+
+	
+
+
+
+
+
+</div>
+
+
+
+</div>
+
+
 	</div>
 
 
