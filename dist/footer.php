c281fa4c81f66cde69f7e5941985ad97272866ae
Louisloode <louisdebraine@gmail.com>
2016-02-21 00:17:56 +0100

    merge last conflicts

--- dist/footer.php
+++ dist/footer.php
@@ -1,5 +1,5 @@
 
-<div>
+<div class="footer">
 
 
 <img src="assets/img/control.svg" class="voiture">
@@ -9,10 +9,10 @@
 
 <img src="assets/img/siege.svg" class="voiture1">
 <img src="assets/img/front.svg" class="voiture2">
-<img src="assets/img/aeration.svg" class="voiture2">
-<img src="assets/img/ventilation.svg" class="voiture2">
-<img src="assets/img/arriere.svg" class="voiture2">
-<img src="assets/img/siege.svg" class="voiture2">
+<img src="assets/img/aeration.svg" class="voiture3">
+<img src="assets/img/ventilation.svg" class="voiture3">
+<img src="assets/img/arriere.svg" class="voiture3">
+<img src="assets/img/siege.svg" class="voiture3">
 <br>
 <p class="control">Control</p>
 
@@ -43,4 +43,55 @@
 
 <!-- <div class="moncercle22  "><p class="txtcercle">On</p></div>
 <div class="moncercle23  "><p class="txtcercle">Off</p></div> -->
+
+
+
+<div class="tiret"></div>
+<div class="tiret2"></div>
+
+<div class="footerbas">
+
+
+<div class="">
+
+
+<div class="boutonv ">
+	<p class="plus">-</p>
+	<p class="contour"><p class="milieu">38</p></p>
+	<p class="moins">+</p>
+	
+</div>
+
+
+</div>
+
+
+
+
+<img src="assets/img/power.svg" class="power1">
+<img src="assets/img/power.svg" class="power2">
+
+
+	<p class="powe">POWER</p>
+
+
+	<p class="sync">SYNC</p>
+
+
+
+	<div class="droitev">
+
+		<div class="boutonv ">
+	<p class="plus">-</p>
+	<p class="contour2"><p class="milieu2">68</p></p>
+	<p class="moins">+</p>
+	
+</div>
+
+	</div>
+
+
+
+</div>
+
 </div>
