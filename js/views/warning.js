d4f052002a935b17a58bbe642eab36e942f1fd8e
benquet <benquet.lucas3@gmail.com>
2016-02-21 23:58:09 +0100

    Warning page: ok

--- js/views/warning.js
+++ js/views/warning.js
@@ -2,6 +2,22 @@
 
 function showWarning() {
         $( "#warning" ).fadeIn('slow');
+
+        $('.bar-percentage[data-percentage]').each(function () {
+		  var progress = $(this);
+		  var percentage = Math.ceil($(this).attr('data-percentage'));
+		  $({countNum: 0}).animate({countNum: percentage}, {
+		    duration: 1500,
+		    easing:'linear',
+		    step: function() {
+		      // What todo on every count
+		      var pct = Math.floor(this.countNum) + '%';
+		      progress.text(pct) && progress.siblings().children().css('width',pct);
+		    }
+		  });
+		});
+
+
 }
 function hideWarning() {
         $( "#warning" ).fadeOut('slow');
@@ -20,3 +36,9 @@ $( ".nav-warning" ).on( "click", function() {
   }
 
 });
+
+
+
+
+
+
