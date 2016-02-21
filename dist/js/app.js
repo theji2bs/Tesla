b4e1e87a2d7b6e66653944962695cfb6aef8da7b
Louisloode <louisdebraine@gmail.com>
2016-02-22 00:27:08 +0100

    Prepare for auth

--- dist/js/app.js
+++ dist/js/app.js
@@ -357,6 +357,43 @@ $( "nav > div > .nav-mail" ).on( "click", function() {
 
 // Base router
 
+function showSettings() {
+		$( "nav" ).fadeOut('fast'); // Header
+		$( "#dash" ).fadeOut('fast'); // Header
+        $( "#settings" ).fadeIn('slow');
+        
+}
+function hideSettings() {
+		$( "#settings" ).fadeOut('fast');
+		$( "nav" ).fadeIn('fast'); // Header
+		$( "#dash" ).fadeIn('fast'); // Header
+        
+    	
+}
+
+$( ".voiture" ).on( "click", function() {
+
+  if( $( this ).hasClass('active') ){ // si l'élément possède la classe .vert
+
+		$( this ).removeClass("active");
+	  	hideSettings()
+	}
+  else{
+	  	$( this ).addClass("active");
+	  	showSettings()
+	  	route = '#settings';
+  }
+
+});
+
+$( "#settings > .close" ).on( "click", function() {
+
+  hideSettings()
+
+});
+
+// Base router
+
 function showTirix() {
         $( "#tirix" ).fadeIn('slow');
 }
---
+++ js/views/settings.js
@@ -0,0 +1,36 @@
+// Base router
+
+function showSettings() {
+		$( "nav" ).fadeOut('fast'); // Header
+		$( "#dash" ).fadeOut('fast'); // Header
+        $( "#settings" ).fadeIn('slow');
+        
+}
+function hideSettings() {
+		$( "#settings" ).fadeOut('fast');
+		$( "nav" ).fadeIn('fast'); // Header
+		$( "#dash" ).fadeIn('fast'); // Header
+        
+    	
+}
+
+$( ".voiture" ).on( "click", function() {
+
+  if( $( this ).hasClass('active') ){ // si l'élément possède la classe .vert
+
+		$( this ).removeClass("active");
+	  	hideSettings()
+	}
+  else{
+	  	$( this ).addClass("active");
+	  	showSettings()
+	  	route = '#settings';
+  }
+
+});
+
+$( "#settings > .close" ).on( "click", function() {
+
+  hideSettings()
+
+});
