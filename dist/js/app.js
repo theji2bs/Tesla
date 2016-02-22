ae9b682be9f5224c996ee32dfc5e563659967418
Louisloode <louisdebraine@gmail.com>
2016-02-22 20:15:41 +0100

    Fix merge tirix

--- dist/js/app.js
+++ dist/js/app.js
@@ -36,12 +36,12 @@ $(document).ready(function(){
     var switchSettingsLights = '';
 
     // Show different elements of the page
-    $( "#tirix" ).fadeIn('slow'); // Dashboard
+    $( "#auth" ).fadeIn('slow'); // Dashboard
     //$( "#settings" ).fadeIn('slow'); // Dashboard
-    $( "header" ).fadeIn('slow'); // Header
-    $( "nav" ).fadeIn('slow'); // Header
-    $( "footer" ).fadeIn('slow'); // Header
-    $( "#dash" ).fadeIn('slow'); // Dashboard
+    //$( "header" ).fadeIn('slow'); // Header
+    //$( "nav" ).fadeIn('slow'); // Header
+    //$( "footer" ).fadeIn('slow'); // Header
+    //$( "#dash" ).fadeIn('slow'); // Dashboard
     
 
 });
