b89298f4318fdb7d21605c82f6aa5947d9385981
benquet <benquet.lucas3@gmail.com>
2016-02-22 01:48:04 +0100

    Authentification ok, See again pad

--- dist/views/auth.php
+++ dist/views/auth.php
@@ -1,3 +1,22 @@
 <section id="auth">
 AUTH
+
+<div class="header">
+	<img src="assets/img/logot.svg" alt="LogoTesla">
+	<p>Welcome Sir,</p>
+</div>
+
+<div class="body">
+	<img src="assets/img/Main.png" class="Authentification" alt="Main">
+</div>
+
+<div class="footer">
+
+	<p class="instru">Start with digital printing or alphanumeric password</p>
+	<div class="container"> 
+	  <div class="row">
+	  	<div class="col-md-3 col-sm-3 col-xs-6"> <a href="#" class="btn btn-sm animated-button victoria-three">To Unclock</a> </div>
+	  </div>
+</div>
+
 </section>
