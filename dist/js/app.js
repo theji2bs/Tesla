90352d5ac52f7deada29d230b6ee7278c17cf987
Louisloode <louisdebraine@gmail.com>
2016-02-24 20:31:42 +0100

    GMerge elhadad ok

--- dist/js/app.js
+++ dist/js/app.js
@@ -23,13 +23,13 @@ $(document).ready(function(){
     var switchSettingsLights = '';
 
     // Show different elements of the page
-    //$( "#auth" ).fadeIn('slow'); // Dashboard
+    $( "#auth" ).fadeIn('slow'); // Dashboard
     //$( "#tirix" ).fadeIn('slow'); // Dashboard
     //$( "#settings" ).fadeIn('slow'); // Dashboard
-    $( "header" ).fadeIn('slow'); // Header
-    $( "nav" ).fadeIn('slow'); // Header
-    $( "footer" ).fadeIn('slow'); // Header
-    $( "#dash" ).fadeIn('slow'); // Dashboard
+    //$( "header" ).fadeIn('slow'); // Header
+    //$( "nav" ).fadeIn('slow'); // Header
+    //$( "footer" ).fadeIn('slow'); // Header
+    //$( "#dash" ).fadeIn('slow'); // Dashboard
     
 
 });
